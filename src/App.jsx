73bf994455d0130d2181ab762bd1c9fdd25d2b0a
krishna <krisnas9792@gmail.com>
2024-07-24 13:31:29 +0530

    adding a header component and writing code inside a buton components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
-import Button from './Components/button';
+// import Button from './Components/Button';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline flex justify-center">Hello world!</h1>
-      <div className='pt-[4.7rem]:lg pt-[5.25rem] overflow-hidden'>
-        <Button className="mt-10 text-black" href="#login">
-          Something
-        </Button>
+      {/* <h1 className="text-3xl font-bold underline flex justify-center text-slate-900">Hello world!</h1> */}
+      <div className='pt-[4.7rem]:lg pt-[5.25rem] overflow-hidden bg-slate-800 text-slate-50' >
+        {/* <Buttonb classgName="my-10" href="#-login">Something</Button> */}
+        <Header />
       </div>
-      <ButtonGradient/>
+      <ButtonGradient />
     </>
   );
 };
 
-export default App
+export default App;
--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -1,12 +1,29 @@
-// import React from 'react';
+import ButtonSvg from '../assets/svg/ButtonSvg';
 
-const button = () => {
-  return (
-    <div>
-        Something
-    </div>
-  )
-}
+const Button = ({ className, children, px, black }) => {
+    console.log('Button Props:', { className, px });
+    const classes = `button relative infinite-flex item-center justify-center h-11 transition-colors
+    hover: text-color-1 ${px || 'px-7'} ${black ? 'text-n-8' : 'text-n-1'} ${className || ''}`;
 
-export default button;
+    const spanClasses = `relative z-10 text-black hover:text-color-1 `
+
+    const renderButton = () => (
+        <button className={classes}>
+            <span className={spanClasses}>{children}</span>
+            {ButtonSvg (black)}
+        </button>
+    );
+
+    // const renderLink = () => (
+    //     <a href={href} className={classes}>
+    //         <span className={spanClasses}>{children}</span>
+    //         {ButtonSvg (black)}
+    //     </a>
+    // );
+
+    // return href ? renderLink() : renderButton();
+    return renderButton();
+};
+
+export default Button;
 
